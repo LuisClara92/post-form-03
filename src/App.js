0c401a13e57c5a75fa9af854f97a4eca9e028c65
LuisClara92 <lc.sbg.92@gmail.com>
2021-01-10 16:04:25 +0000

    create repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MovieForm from "./components/MovieForm";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <MovieForm />
+    </div>
+  );
+}
